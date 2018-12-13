15fcd40acb5e530d535fce594dd211d503507e95
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-12-13 08:15:19 +0100

    forgot the plugin, sorry :)

---
+++ ShowCharacter.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,119 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class ShowCharacter(ReporterPlugin):
+
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': u'Character',
+			'de': u'Zeichen',
+			'fr': u'caractère',
+			'zh': u'👩‍🏫参考字',
+		})
+	
+	def drawMultipleTextAtPoint(self, text, textPosition, fontSize=10.0, fontColor=NSColor.blackColor(), align='left', fontNames=() ):
+		try:
+			alignment = {
+				'topleft': 6, 'topcenter': 7, 'topright': 8,
+				'left': 3, 'center': 4, 'right': 5,
+				'bottomleft': 0, 'bottomcenter': 1, 'bottomright': 2,
+			}
+			
+			textAlignment = alignment[align]
+			currentZoom = self.getScale()
+			
+			# see if the font names are among the installed fonts:
+			verifiedFonts = []
+			for fontName in fontNames:
+				font = NSFont.fontWithName_size_(fontName, fontSize / currentZoom)
+				if font:
+					verifiedFonts.append(font)
+			
+			# if not, use system font as fallback:
+			if not verifiedFonts:
+				systemFont = NSFont.labelFontOfSize_(fontSize / currentZoom)
+				verifiedFonts.append(systemFont)
+			
+			for font in verifiedFonts:
+				fontAttributes = {
+					NSFontAttributeName: font,
+					NSForegroundColorAttributeName: fontColor,
+				}
+				displayText = NSAttributedString.alloc().initWithString_attributes_(unicode(text), fontAttributes)
+				displayText.drawAtPoint_alignment_(textPosition, textAlignment)
+				advanceWidth = displayText.size().width
+				x,y = textPosition
+				textPosition = (x+advanceWidth, y)
+		except:
+			import traceback
+			print traceback.format_exc()
+	
+		
+	def foreground(self, layer):
+		glyph = layer.parent
+		character = glyph.glyphInfo.unicharString()
+		
+		# try again if glyph has no unicode character associated with it:
+		if not character and "." in glyph.name:
+			nameWithoutSuffix = glyph.name[:glyph.name.find(".")]
+			glyphInfo = Glyphs.glyphInfoForName(nameWithoutSuffix)
+			character = glyphInfo.unicharString()
+			
+		if character:
+			font = Glyphs.font
+			tab = font.currentTab
+			if tab.scale > 0.1999:
+				master = layer.associatedFontMaster()
+				y = max(master.ascender, layer.bounds.origin.y+layer.bounds.size.height+50.0)
+		
+				fontSize = 100.0
+				fontNames = []
+				extraFontNames = font.customParameters["Sample Fonts"]
+				if extraFontNames:
+					if ";" in extraFontNames:
+						# there is a fontsize passed as well:
+						parsedParameter = extraFontNames.split(";")
+						extraFontNames = parsedParameter[0].strip()
+						try:
+							parsedFontSize = float(parsedParameter[1].strip())
+							if parsedFontSize > 0.0:
+								fontSize = parsedFontSize
+						except:
+							pass
+				
+					fontNames += [name.strip() for name in extraFontNames.split(",") if name]
+			
+				self.drawMultipleTextAtPoint(
+					character, NSPoint(0,y), 
+					fontSize=fontSize, 
+					fontColor=NSColor.colorWithRed_green_blue_alpha_(0.1, 0.4, 0.8, 0.6),
+					fontNames=fontNames,
+					align="bottomleft",
+					)
+
+	# def inactiveLayer(self, layer):
+	# 	NSColor.redColor().set()
+	# 	if layer.paths:
+	# 		layer.bezierPath.fill()
+	# 	if layer.components:
+	# 		for component in layer.components:
+	# 			component.bezierPath.fill()
+
+
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
